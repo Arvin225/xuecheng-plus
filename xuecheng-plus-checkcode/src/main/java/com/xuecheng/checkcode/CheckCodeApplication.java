2c2655e4d25622a3edb2af8fd9376a376540823a
Arvin <60811406+Arvin225@users.noreply.github.com>
2023-04-23 16:40:48 +0800

    添加SpringBoot打包插件

--- xuecheng-plus-checkcode/src/main/java/com/xuecheng/checkcode/CheckCodeApplication.java
+++ xuecheng-plus-checkcode/src/main/java/com/xuecheng/checkcode/CheckCodeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheckcodeApplication {
+public class CheckCodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CheckcodeApplication.class, args);
+        SpringApplication.run(CheckCodeApplication.class, args);
     }
 
 }
